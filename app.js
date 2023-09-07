c893495679a471e6636e73987bb2a352bca3a45b
osamaaaaaaaaa <106319313+osamaaaaaaaaa@users.noreply.github.com>
2023-09-07 19:52:56 +0300

    i

--- app.js
+++ app.js
@@ -1,61 +1,48 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+const stripe = require("stripe")("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");
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+exports.stripePaymentIntentRequest = functions.https.onRequest(async (req, res) => {
+    try {
+        let customerId;
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
+        //Gets the customer who's email id matches the one sent by the client
+        const customerList = await stripe.customers.list({
+            email: req.body.email,
+            limit: 1
         });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+                
+        //Checks the if the customer exists, if not creates a new customer
+        if (customerList.data.length !== 0) {
+            customerId = customerList.data[0].id;
+        }
+        else {
+            const customer = await stripe.customers.create({
+                email: req.body.email
+            });
+            customerId = customer.data.id;
+        }
+
+        //Creates a temporary secret key linked with the customer 
+        const ephemeralKey = await stripe.ephemeralKeys.create(
+            { customer: customerId },
+            { apiVersion: '2020-08-27' }
+        );
+
+        //Creates a new payment intent with amount passed in from the client
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: parseInt(req.body.amount),
+            currency: 'usd',
+            customer: customerId,
+        })
+
+        res.status(200).send({
+            paymentIntent: paymentIntent.client_secret,
+            ephemeralKey: ephemeralKey.secret,
+            customer: customerId,
+            success: true,
+        })
+        
+    } catch (error) {
+        res.status(404).send({ success: false, error: error.message })
+    }
+});
